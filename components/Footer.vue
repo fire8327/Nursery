424dd6c81d57eb48bbb91974dfe472f8e6d63ce4
fire8327 <114702513+fire8327@users.noreply.github.com>
2024-04-02 05:19:09 +0300

    create footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="grid-container mt-14">
+        <div class="flex flex-col items-center py-5 md:py-8 xl:py-10 gap-5 md:gap-8 xl:gap-10">
+            <NuxtLink to="/" class="flex flex-col text-[#869D8B] Goma items-center">
+                <span class="text-xs md:text-base xl:text-lg">Детский сад 348</span>
+                <span class="text-[6px] md:text-[8px] xl:text-[10px]">Советского района Волгограда</span>
+            </NuxtLink>
+            <div class="flex items-center max-lg:flex-col gap-2 md:gap-6 xl:gap-9 w-fit font-Nunito text-sm md:text-base xl:text-lg">
+                <NuxtLink to="/">Главная</NuxtLink>
+                <NuxtLink to="/">Объявления и Новости</NuxtLink>
+                <NuxtLink to="/">Доп. услуги</NuxtLink>
+                <NuxtLink to="/">Питание</NuxtLink>
+                <NuxtLink to="/">Наши успехи</NuxtLink>  
+            </div>
+            <div class="w-full h-px bg-[#EEF0F3]"></div>
+            <p class="text-sm md:text-lg xl:text-xl font-medium text-center">© 2024. Детский сад №348 Советского района Волгограда</p>
+        </div>
+    </footer>
+</template>
